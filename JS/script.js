ea7f8433b95015617fab72f74228addd8eae6b01
hannahbeatrz <hannahbeatriz1995@gmail.com>
2024-01-30 00:47:00 +0800

    di pa tapos na js + scale sa buttons

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll('.button');
+    const boxes = document.querySelector('.boxes');
+    boxes.style.borderColor = 'white';
+  
+    buttons.forEach(button => {
+      button.addEventListener('click', function () {
+        const color = this.getAttribute('border-color');
+        boxes.style.borderColor = color;
+        setTimeout(function() {
+            boxes.style.borderColor = color;
+          }, 1000);
+      });
+    });
+  });
