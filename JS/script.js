3762305f563d02173f1dfd286c791859b12eed95
hannahbeatrz <hannahbeatriz1995@gmail.com>
2024-01-30 01:06:07 +0800

    bat paganon

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll('.button');
-    const boxes = document.querySelector('.boxes');
-    boxes.style.borderColor = 'transparent';
-  
-    buttons.forEach(button => {
+  const buttons = document.querySelectorAll('.button');
+  const boxes = document.querySelector('.boxes');
+  boxes.style.borderColor = 'transparent';
+
+  buttons.forEach(button => {
       button.addEventListener('click', function () {
-        const color = this.getAttribute('border-color');
-        boxes.style.borderColor = color;
-        setTimeout(function() {
-            boxes.style.borderColor = color;
-          }, 1000);
+          const color = this.getAttribute('border-color');
+          slideUp(boxes); 
+          setTimeout(function () {
+              boxes.style.borderColor = color;
+              slideDown(boxes); 
+          }, 500); 
+
       });
-      
-    });
   });
+
+  function slideDown(element) {
+      element.style.transition = 'height 0.5s ease';
+      setTimeout(() => {
+          element.style.height = '500px';
+      }, 50);
+  }
+
+  function slideUp(element) {
+      element.style.transition = 'height 0.5s ease';
+      element.style.height = '0';
+  }
+});
